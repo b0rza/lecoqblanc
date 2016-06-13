e9e45028ae89181b672d337541be80dcecf4e605
Ante Borzić <aborzic@extensionengine.com>
2016-06-13 16:04:26 +0200

    Start adding css

---
+++ src/components/app/app.vue
@@ -0,0 +1,43 @@
+<script>
+import template from './template.html';
+import Diner from 'components/diner/diner';
+
+export default {
+  template,
+
+  // Hooks
+  // ---------------------------------------
+  created() {
+    this.diners = this.getDiners();
+  },
+
+  // Components
+  // ---------------------------------------
+  components: {
+    Diner,
+  },
+
+  // Models
+  // ---------------------------------------
+  data() {
+    return {
+      diners: [],
+    };
+  },
+
+  // Methods
+  // ---------------------------------------
+  methods: {
+    getDiners() {
+      return [{
+        name: 'Le Coq Blanc',
+        address: 'Jurasova 970',
+        contacts: ['099-201-3333'],
+        hours: ['09-23', '10-22'],
+      }];
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss" src="./style.scss"></style>
---
+++ src/components/diner/diner.vue
@@ -0,0 +1,13 @@
+<script>
+  import template from './template.html';
+
+  export default {
+    template,
+    data() {
+      return {
+        name: 'Le Coq Blanc',
+      };
+    },
+  };
+</script>
+<style lang="scss" src="./style.scss"></style>
